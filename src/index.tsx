fb92bcd958bffd97e3f9ff4365b277c71bb3ba4b
Filip Chalupa <chalupa.filip@gmail.com>
2022-04-22 15:09:29 +0200

    Add button base

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,80 @@
-import React, { FunctionComponent } from 'react'
+import Link, { LinkProps as NextLinkProps } from 'next/link'
+import React, {
+	ButtonHTMLAttributes,
+	DetailedHTMLProps,
+	FunctionComponent,
+	HTMLAttributes,
+} from 'react'
+import { assertNever } from './utils/assertNever'
 
-export const ButtonBase: FunctionComponent = () => {
-	return <>@TODO</>
+type ButtonLinkProps = {
+	type: 'link'
+} & NextLinkProps
+
+type ButtonButtonProps = {
+	type: 'button'
+	onClick?: React.MouseEventHandler<HTMLButtonElement>
+} & Pick<
+	DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
+	'onClick' | 'disabled'
+>
+
+type ButtonSubmitProps = Omit<ButtonButtonProps, 'type'> & {
+	type: 'submit'
+}
+
+type ButtonResetProps = Omit<ButtonButtonProps, 'type'> & {
+	type: 'reset'
+}
+
+type SharedProps = {
+	className?: string
+	children?: React.ReactNode
+} & Pick<HTMLAttributes<HTMLAnchorElement | HTMLButtonElement>, 'id'>
+
+export type ButtonBaseProps = SharedProps &
+	(ButtonLinkProps | ButtonButtonProps | ButtonSubmitProps | ButtonResetProps)
+
+export const ButtonBase: FunctionComponent<ButtonBaseProps> = ({
+	children,
+	className,
+	id,
+	...otherProps
+}) => {
+	const commonProps = { className, id }
+
+	if (
+		otherProps.type === 'button' ||
+		otherProps.type === 'submit' ||
+		otherProps.type === 'reset'
+	) {
+		return (
+			<button
+				{...commonProps}
+				type={otherProps.type}
+				onClick={otherProps.onClick}
+				disabled={otherProps.disabled}
+			>
+				{children}
+			</button>
+		)
+	} else if (otherProps.type === 'link') {
+		return (
+			<Link
+				href={otherProps.href}
+				as={otherProps.as}
+				replace={otherProps.replace}
+				scroll={otherProps.scroll}
+				shallow={otherProps.shallow}
+				passHref={otherProps.passHref}
+				prefetch={otherProps.prefetch}
+				locale={otherProps.locale}
+			>
+				<a {...commonProps}>{children}</a>
+			</Link>
+		)
+	} else {
+		assertNever(otherProps)
+	}
+	return null
 }
--- src/stories/Example.tsx
+++ src/stories/Example.tsx
@@ -1,9 +1,68 @@
 import React, { FunctionComponent } from 'react'
 import { ButtonBase } from '..'
+import { ButtonBaseProps } from '../index'
 import './global.css'
 
-export interface ExampleProps {}
+type ExampleButtonProps = {
+	variant?: 'primary' | 'secondary'
+	size?: 'small' | 'medium'
+} & ButtonBaseProps
 
-export const Example: FunctionComponent<ExampleProps> = () => {
-	return <ButtonBase />
+const ExampleButton: FunctionComponent<ExampleButtonProps> = ({
+	variant = 'primary',
+	size = 'medium',
+	...props
+}) => {
+	const className = `button button--variant-${variant} button--size-${size}`
+
+	return <ButtonBase className={className} {...props} />
+}
+
+export const Example: FunctionComponent = () => {
+	return (
+		<>
+			<h1>Button example</h1>
+			<h2>
+				Link <code>{'<a href="https://example.com">'}</code>
+			</h2>
+			<ExampleButton type="link" href="https://example.com">
+				Link
+			</ExampleButton>
+			<h2>
+				Button <code>{'<button type="button">'}</code>
+			</h2>
+			<ExampleButton
+				type="button"
+				onClick={() => {
+					alert('Click')
+				}}
+			>
+				Button
+			</ExampleButton>
+			<h2>
+				Button <code>{'<button type="submit">'}</code> and{' '}
+				<code>{'<button type="reset">'}</code>
+			</h2>
+			<form
+				onSubmit={(event) => {
+					event.preventDefault()
+					alert('Submit')
+				}}
+			>
+				<label>
+					Dummy input: <input />
+				</label>
+				<br />
+				<br />
+				<div>
+					<ExampleButton type="submit" variant="primary" size="small">
+						Submit
+					</ExampleButton>{' '}
+					<ExampleButton type="reset" variant="secondary" size="small">
+						Reset
+					</ExampleButton>
+				</div>
+			</form>
+		</>
+	)
 }
