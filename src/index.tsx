d81783973483a0c9d7b82e5c350a2b1619a6951c
Filip Chalupa <chalupa.filip@gmail.com>
2022-09-30 19:02:13 +0200

    Fix ref

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,9 @@ import React, {
 	AnchorHTMLAttributes,
 	ButtonHTMLAttributes,
 	DetailedHTMLProps,
-	FunctionComponent,
+	forwardRef,
 	HTMLAttributes,
+	LegacyRef,
 } from 'react'
 import { assertNever } from './utils/assertNever'
 
@@ -18,14 +19,14 @@ type ButtonLinkProps = {
 			AnchorHTMLAttributes<HTMLAnchorElement>,
 			HTMLAnchorElement
 		>,
-		'ref' | 'target' | 'rel'
+		'target' | 'rel'
 	>
 
 type ButtonButtonProps = {
 	type: 'button'
 } & Pick<
 	DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>,
-	'ref' | 'disabled'
+	'disabled'
 >
 
 type ButtonSubmitProps = Omit<ButtonButtonProps, 'type'> & {
@@ -52,65 +53,73 @@ export type SharedBaseButtonProps = {
 
 export type ButtonBaseProps = SharedBaseButtonProps & DistinctBaseButtonProps
 
-export const ButtonBase: FunctionComponent<ButtonBaseProps> = ({
-	children,
-	className,
-	id,
-	tabIndex,
-	onClick,
-	onPointerDown,
-	style,
-	title,
-	...otherProps
-}) => {
-	const commonProps = {
-		className,
-		id,
-		tabIndex,
-		onClick,
-		onPointerDown,
-		style,
-		title,
-	}
-
-	if (
-		otherProps.type === 'button' ||
-		otherProps.type === 'submit' ||
-		otherProps.type === 'reset'
-	) {
-		return (
-			<button
-				{...commonProps}
-				type={otherProps.type}
-				disabled={otherProps.disabled}
-				ref={otherProps.ref}
-			>
-				{children}
-			</button>
-		)
-	} else if (otherProps.type === 'link') {
-		const anchorProps = {
-			...commonProps,
-			target: otherProps.target,
-			rel: otherProps.rel,
-			ref: otherProps.ref,
+export const ButtonBase = forwardRef<
+	HTMLButtonElement | HTMLAnchorElement /* @TODO: respect type prop */,
+	ButtonBaseProps
+>(
+	(
+		{
+			children,
+			className,
+			id,
+			tabIndex,
+			onClick,
+			onPointerDown,
+			style,
+			title,
+			...otherProps
+		},
+		ref,
+	) => {
+		const commonProps = {
+			className,
+			id,
+			tabIndex,
+			onClick,
+			onPointerDown,
+			style,
+			title,
 		}
-		return (
-			<Link
-				href={otherProps.href}
-				as={otherProps.as}
-				replace={otherProps.replace}
-				scroll={otherProps.scroll}
-				shallow={otherProps.shallow}
-				passHref={otherProps.passHref}
-				prefetch={otherProps.prefetch}
-				locale={otherProps.locale}
-			>
-				<a {...anchorProps}>{children}</a>
-			</Link>
-		)
-	} else {
-		assertNever(otherProps)
-	}
-	return null
-}
+
+		if (
+			otherProps.type === 'button' ||
+			otherProps.type === 'submit' ||
+			otherProps.type === 'reset'
+		) {
+			return (
+				<button
+					{...commonProps}
+					type={otherProps.type}
+					disabled={otherProps.disabled}
+					ref={ref as LegacyRef<HTMLButtonElement>}
+				>
+					{children}
+				</button>
+			)
+		} else if (otherProps.type === 'link') {
+			const anchorProps = {
+				...commonProps,
+				target: otherProps.target,
+				rel: otherProps.rel,
+				ref: ref as LegacyRef<HTMLAnchorElement>,
+			}
+			return (
+				<Link
+					href={otherProps.href}
+					as={otherProps.as}
+					replace={otherProps.replace}
+					scroll={otherProps.scroll}
+					shallow={otherProps.shallow}
+					passHref={otherProps.passHref}
+					prefetch={otherProps.prefetch}
+					locale={otherProps.locale}
+				>
+					<a {...anchorProps}>{children}</a>
+				</Link>
+			)
+		} else {
+			assertNever(otherProps)
+		}
+		return null
+	},
+)
